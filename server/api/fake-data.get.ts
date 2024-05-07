db654bcaf555a7994cce977729e75f41d0f8787f
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-05-07 22:42:33 +0300

    removed unused conditions

--- server/api/fake-data.get.ts
+++ server/api/fake-data.get.ts
@@ -5,14 +5,6 @@ export default defineEventHandler(async (event) => {
   const prevCursor = +(query.page ?? 0) - 1;
   const nextCursor = +(query.page ?? 0) + 1;
 
-  if (prevCursor < 0) {
-    return { items: [], nextCursor, prevCursor: 0 }
-  }
-
-  if (nextCursor > 1000) {
-    return { items: [], nextCursor: 1000, prevCursor }
-  }
-
   const items = getItems(25);
 
   return { items, nextCursor, prevCursor }
