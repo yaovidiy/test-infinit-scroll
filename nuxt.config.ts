af98245833f9f488b8bc863c11ece81458e6cb8f
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-05-07 19:17:27 +0300

    added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@hebilicious/vue-query-nuxt"]
+  modules: ["@hebilicious/vue-query-nuxt"],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
