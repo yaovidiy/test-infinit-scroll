ac646e87d319e68e8c442cb0beec64d964deb670
Yaroslav Ovdii <yaovdiy@gmail.com>
2024-05-07 20:36:49 +0300

    added vue use utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@hebilicious/vue-query-nuxt"],
+  modules: ["@hebilicious/vue-query-nuxt", "@vueuse/nuxt",],
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
